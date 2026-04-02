e27684fd62159b86e4672d718f208a371a516f63
luke <hey@luke.town>
2026-04-02 15:01:48 -0700

    restyled with darker colors

--- src/app/create/[matchId]/components/ContractsList.tsx
+++ src/app/create/[matchId]/components/ContractsList.tsx
@@ -10,14 +10,19 @@ export default function ContractsList({
             {contracts.map((contract) => (
                 <details
                     key={contract.id}
-                    className="group bg-slate-600 w-full max-w-96 border-2 border-slate-400 select-none"
+                    className="bg-slate-600 w-full max-w-96 border-2 border-slate-600 select-none"
                 >
-                    <summary className="p-1 font-bold group-[:open]:border-b-2 border-slate-400 cursor-default">
+                    <summary
+                        className="p-1 font-bold cursor-default bg-center "
+                        style={{
+                            backgroundImage: `linear-gradient(to right, oklch(from var(--color-slate-800) l c h), oklch(from var(--color-slate-600) l c h / 40%)),url(/${contract.location}_background.webp)`,
+                        }}
+                    >
                         {FormatContractLocation(contract.location)}
                     </summary>
 
                     <ul>
-                        <li className="p-1 bg-slate-600">
+                        <li className="p-1 bg-slate-800">
                             <div className="flex gap-1 items-center text-sm">
                                 <p>{"Epic:"}</p>
                             </div>
@@ -33,7 +38,7 @@ export default function ContractsList({
                                 {contract.steamId ?? "No contract id"}
                             </p>
                         </li>
-                        <li className="p-1 bg-slate-600">
+                        <li className="p-1 bg-slate-800">
                             <div className="flex gap-1 items-center text-sm">
                                 <p>{"PlayStation:"}</p>
                             </div>
@@ -49,7 +54,7 @@ export default function ContractsList({
                                 {contract.xboxId ?? "No contract id"}
                             </p>
                         </li>
-                        <li className="p-1 bg-slate-600">
+                        <li className="p-1 bg-slate-800">
                             <div className="flex gap-1 items-center text-sm">
                                 <p>{"Nintendo Switch:"}</p>
                             </div>
