9c0a91eb96a4a08d0118ea6f9eb414f2dfd1ea41
luke <hey@luke.town>
2026-04-06 16:46:18 -0700

    made details content absolute + seperate component

--- src/app/create/[matchId]/components/ContractsList.tsx
+++ src/app/create/[matchId]/components/ContractsList.tsx
@@ -6,14 +6,15 @@ export default function ContractsList({
     contracts: Contract[];
 }) {
     return (
-        <div className="flex flex-col gap-2">
+        <div className="max-w-195 grid lg:grid-cols-2 grid-cols-1 gap-2 align-baseline">
             {contracts.map((contract) => (
                 <details
                     key={contract.id}
-                    className="bg-slate-600 w-full max-w-96 border-2 border-slate-600 select-none"
+                    name="contract"
+                    className="relative w-full max-w-96 bg-slate-700 border-2 border-slate-700 select-none sm:text-base text-sm"
                 >
                     <summary
-                        className="p-1 font-bold cursor-default bg-center "
+                        className="h-full p-1 font-bold cursor-default bg-center"
                         style={{
                             backgroundImage: `linear-gradient(to right, oklch(from var(--color-slate-800) l c h), oklch(from var(--color-slate-600) l c h / 40%)),url(/${contract.location}_background.webp)`,
                         }}
@@ -21,50 +22,50 @@ export default function ContractsList({
                         {FormatContractLocation(contract.location)}
                     </summary>
 
-                    <ul>
-                        <li className="p-1 bg-slate-800">
-                            <div className="flex gap-1 items-center text-sm">
-                                <p>{"Epic:"}</p>
-                            </div>
-                            <p className="select-text">
-                                {contract.epicId ?? "No contract id"}
-                            </p>
-                        </li>
-                        <li className="p-1 bg-slate-700">
-                            <div className="flex gap-1 items-center text-sm">
-                                <p>{"Steam:"}</p>
-                            </div>
-                            <p className="select-text">
-                                {contract.steamId ?? "No contract id"}
-                            </p>
-                        </li>
-                        <li className="p-1 bg-slate-800">
-                            <div className="flex gap-1 items-center text-sm">
-                                <p>{"PlayStation:"}</p>
-                            </div>
-                            <p className="select-text">
-                                {contract.playstationId ?? "No contract id"}
-                            </p>
-                        </li>
-                        <li className="p-1 bg-slate-700">
-                            <div className="flex gap-1 items-center text-sm">
-                                <p>{"Xbox:"}</p>
-                            </div>
-                            <p className="select-text">
-                                {contract.xboxId ?? "No contract id"}
-                            </p>
-                        </li>
-                        <li className="p-1 bg-slate-800">
-                            <div className="flex gap-1 items-center text-sm">
-                                <p>{"Nintendo Switch:"}</p>
-                            </div>
-                            <p className="select-text">
-                                {contract.switchId ?? "No contract id"}
-                            </p>
-                        </li>
+                    <ul className="absolute w-full z-10 top-full -left-0.5 border-2 border-slate-600 border-t-0 box-content">
+                        <ContractIdEntry
+                            id={contract.epicId}
+                            platform={"Epic"}
+                        />
+                        <ContractIdEntry
+                            id={contract.steamId}
+                            platform={"Steam"}
+                        />
+                        <ContractIdEntry
+                            id={contract.playstationId}
+                            platform={"PlayStation"}
+                        />
+                        <ContractIdEntry
+                            id={contract.xboxId}
+                            platform={"Xbox"}
+                        />
+                        <ContractIdEntry
+                            id={contract.switchId}
+                            platform={"Switch"}
+                        />
                     </ul>
                 </details>
             ))}
         </div>
+    );
+}
+
+function ContractIdEntry({
+    id,
+    platform,
+}: {
+    id: string | undefined;
+    platform: "Epic" | "Steam" | "PlayStation" | "Xbox" | "Switch";
+}) {
+    return (
+        <li className="p-0.5 odd:bg-slate-800 even:bg-slate-700">
+            <p className="text-xs">{platform}</p>
+            {id !== undefined && (
+                <p className="select-text align-text-top">{id}</p>
+            )}
+            {id === undefined && (
+                <p className="select-text italic">{"No contract ID"}</p>
+            )}
+        </li>
     );
 }
