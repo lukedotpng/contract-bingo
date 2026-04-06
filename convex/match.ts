9da70bf1e99ff5a509d8ff833ea7bbfd3c11a686
Onteia <onteialexa@gmail.com>
2026-04-06 12:52:29 -0700

    Add join uri to matches

--- convex/match.ts
+++ convex/match.ts
@@ -2,6 +2,7 @@ import { query } from "./_generated/server";
 import { mutation } from "./_generated/server";
 import { v } from "convex/values";
 import { Status } from "./status";
+import { Id } from "./_generated/dataModel";
 
 export const createMatch = mutation({
     args: {
@@ -74,5 +75,39 @@ export const deleteMatch = mutation({
         await ctx.db.delete("match", args.matchId);
 
         return Status.OK;
+    },
+});
+
+function joinURI(matchId: Id<"match">, teamId: Id<"team">): string {
+    const BASE_JOIN_URI = "play";
+
+    return `/${BASE_JOIN_URI}/${matchId}/${teamId}`;
+}
+
+export const getAllJoinURIs = query({
+    args: {
+        matchId: v.id("match"),
+    },
+    handler: async (ctx, args) => {
+        const match = await ctx.db.get("match", args.matchId);
+        if (match == null) return Status.NOT_FOUND;
+
+        return match.teamIds.map(teamId => joinURI(args.matchId, teamId));
+    },
+});
+
+export const getJoinURI = mutation({
+    args: {
+        matchId: v.id("match"),
+        teamId: v.id("team"),
+    },
+    handler: async (ctx, args) => {
+        const match = await ctx.db.get("match", args.matchId);
+        if (match == null) return Status.NOT_FOUND;
+
+        const team = await ctx.db.get("team", args.teamId);
+        if (team == null) return Status.NOT_FOUND;
+
+        return joinURI(args.matchId, args.teamId);
     },
 });
