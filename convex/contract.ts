c7e19bffe0461d7c7acc3f0dabb6994a6fe6139b
luke <hey@luke.town>
2026-04-25 13:50:23 -0700

    added createSingleContractWithBoard mutation

--- convex/contract.ts
+++ convex/contract.ts
@@ -65,6 +65,58 @@ export const createBulkContractsWithBoard = mutation({
     },
 });
 
+export const createSingleContractWithBoard = mutation({
+    args: {
+        boardId: v.id("board"),
+        contract: v.object({
+            epicId: v.optional(v.string()),
+            steamId: v.optional(v.string()),
+            xboxId: v.optional(v.string()),
+            playstationId: v.optional(v.string()),
+            switchId: v.optional(v.string()),
+            location: v.union(
+                v.literal("freeform_training"),
+                v.literal("the_final_test"),
+                v.literal("the_showstopper"),
+                v.literal("world_of_tomorrow"),
+                v.literal("a_gilded_cage"),
+                v.literal("club_27"),
+                v.literal("freedom_fighters"),
+                v.literal("situs_inversus"),
+                v.literal("nightcall"),
+                v.literal("the_finish_line"),
+                v.literal("three-headed_serpent"),
+                v.literal("chasing_a_ghost"),
+                v.literal("another_life"),
+                v.literal("shadows_in_the_water"),
+                v.literal("the_ark_society"),
+                v.literal("golden_handshake"),
+                v.literal("the_last_resort"),
+                v.literal("on_top_of_the_world"),
+                v.literal("death_in_the_family"),
+                v.literal("apex_predator"),
+                v.literal("end_of_an_era"),
+                v.literal("the_farewell"),
+                v.literal("holiday_hoarders"),
+                v.literal("landslide"),
+                v.literal("the_icon"),
+                v.literal("the_author"),
+                v.literal("a_house_built_on_sand"),
+                v.literal("the_source"),
+                v.literal("patient_zero"),
+                v.literal("hokkaido_snow_festival"),
+                v.literal("the_dartmoor_garden_show"),
+            ),
+        }),
+    },
+    handler: async (ctx, args) => {
+        const contractId = await createContractHelper(ctx, args.contract);
+        if (contractId !== null) {
+            await createBoardToContractHelper(ctx, args.boardId, contractId);
+        }
+    },
+});
+
 async function createContractHelper(
     ctx: MutationCtx,
     contract: {
